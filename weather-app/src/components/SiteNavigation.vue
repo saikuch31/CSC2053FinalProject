17588d1d405d9e1ee4c14d89d0854aca482a85b1
Sai Kuchulakanti <saikuchulakanti@gmail.com>
2023-12-06 15:49:17 -0500

    completed until section 13

--- weather-app/src/components/SiteNavigation.vue
+++ weather-app/src/components/SiteNavigation.vue
@@ -1,29 +1,31 @@
 <template>
-    <header class="sticky top-0 bg-weather-primary shadow-lg">
-        <nav class="container flex flex-col sm:flex-row
-        items-center gap-4 text-white py-6"
-        >
-        <RouterLink :to="{name: 'home'}">
-            <div class="flex items-center gap-3">
-                <i class="fa-solid fa-sun text-2xl"></i>
-                <p class="text-2xl">The Local Weather</p>
-            </div>
-        </RouterLink>
-
-        <div class="flex gap-3 flex-1 justify-end">
-            <i class="fa-solid fa-circle-info text-xl
-            hover:text-weather-secondary duration-150
-            cursor-pointer"
-            @click="toggleModal"
-            ></i>
-
-            <i class="fa-solid fa-plus text-xl
-            hover:text-weather-secondary duration-150
-            cursor-pointer"></i>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav
+      class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
         </div>
+      </RouterLink>
 
-        <BaseModal :modalActive="modalActive"
-        @close-modal="toggleModal">
+      <div class="flex gap-3 flex-1 justify-end">
+        <i
+          class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="toggleModal"
+        ></i>
+        <i
+          class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"
+          @click="addCity"
+          v-if="route.query"
+        ></i>
+      </div>
+
+      <BaseModal
+        :modalActive="modalActive"
+        @close-modal="toggleModal"
+      >
         <div class="text-black">
           <h1 class="text-2xl mb-1">About:</h1>
           <p class="mb-4">
@@ -56,16 +58,49 @@
         </div>
       </BaseModal>
     </nav>
-    </header>
+  </header>
 </template>
 
 <script setup>
+import { RouterLink, useRoute, useRouter } from "vue-router";
+import { uid } from "uid";
 import { ref } from "vue";
-import { RouterLink } from "vue-router";
 import BaseModal from "./BaseModal.vue";
+
+const savedCities = ref([]);
+const route = useRoute();
+const router = useRouter();
+const addCity = () => {
+  if (localStorage.getItem("savedCities")) {
+    savedCities.value = JSON.parse(
+      localStorage.getItem("savedCities")
+    );
+  }
+
+  const locationObj = {
+    id: uid(),
+    state: route.params.state,
+    city: route.params.city,
+    coords: {
+      lat: route.query.lat,
+      lng: route.query.lng,
+    },
+  };
+
+  savedCities.value.push(locationObj);
+  localStorage.setItem(
+    "savedCities",
+    JSON.stringify(savedCities.value)
+  );
+
+  let query = Object.assign({}, route.query);
+  delete query.preview;
+  query.id = locationObj.id;
+  router.replace({ query });
+};
 
 const modalActive = ref(null);
 const toggleModal = () => {
-    modalActive.value = !modalActive.value;
+  modalActive.value = !modalActive.value;
 };
 </script>
